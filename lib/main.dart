f867aa80c4e5b311c2679fa293706236920265c1
C-JR1987 <juniorfilho639@gmail.com>
2025-03-20 21:16:53 -0300

    atividade 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,127 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(HomePage());
+  runApp(TinderPage());
+}
+
+class TinderPage extends StatelessWidget {
+  const TinderPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [Color(0xFFFF5F6D), Color.fromARGB(255, 255, 0, 85)],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              const Spacer(),
+              // Logo Tinder
+              Column(
+                children: [Icon(Icons.whatshot, color: Colors.white, size: 70)],
+              ),
+              const SizedBox(height: 40),
+              Text(
+                "By tapping Create Account or Sign In, you agree to our\nTerms. Learn how we process your data in our Privacy policy and Cookies Policy.",
+                textAlign: TextAlign.center,
+                style: TextStyle(color: Colors.white70, fontSize: 12),
+              ),
+              const SizedBox(height: 40),
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Color.fromARGB(255, 0, 0, 0), // Cor do botão Apple
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      margin: EdgeInsets.only(right: 10),
+                      child: Image.network(
+                        'https://img.icons8.com/m_sharp/512/FFFFFF/mac-os.png',
+                      ),
+                    ),
+                    Text(
+                      "SIGN IN WITH APPLE",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              // Botão Facebook
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.blue, // Cor do botão Facebook
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      margin: EdgeInsets.only(right: 10),
+                      child: Image.network(
+                        'https://img.icons8.com/m_rounded/512/FFFFFF/facebook-new.png',
+                      ),
+                    ),
+                    Text(
+                      "SIGN IN WITH FACEBOOK",
+                      style: TextStyle(fontSize: 16, color: Colors.white),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 10),
+              // Botão Phone Number
+              Container(
+                padding: EdgeInsets.symmetric(horizontal: 80, vertical: 10),
+                decoration: BoxDecoration(
+                  color: Colors.white, // Cor do botão Phone
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                child: Row(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    Container(
+                      width: 30,
+                      height: 30,
+                      margin: EdgeInsets.only(right: 10),
+                      child: Image.network(
+                        'https://upload.wikimedia.org/wikipedia/commons/8/85/Phone_icon.svg',
+                      ),
+                    ),
+                    Text(
+                      "SIGN IN WITH PHONE NUMBER",
+                      style: TextStyle(fontSize: 16, color: Colors.black),
+                    ),
+                  ],
+                ),
+              ),
+              const SizedBox(height: 20),
+              Text(
+                "Trouble Signing In?",
+                style: TextStyle(color: Colors.white),
+              ),
+              const Spacer(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
 }
 
 class HomePage extends StatelessWidget {
